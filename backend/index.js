61ce684a6e116bf198202571d1618cf018f010c4
Wasifkalimkalim <wasifnit2k19@gmail.com>
2024-09-15 00:16:28 +0530

    Model Added All

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const app = express();
 
+
+// All import route 
+const courseRoute = require('./routes/coursesRoute');
+const userRoute = require("./routes/userRoute"); 
+
+// DB connection
 require("dotenv").config();
 const dbConnect = require("./config/db");
 dbConnect();
 
+
+// Routes Mapping
+app.use('/courses/api', courseRoute);
+app.user('/user/api',userRoute);
+
+
+// Server Connect
 const PORT= process.env.PORT ||3000;
 
 app.get('/', (req, res)=>{
-    res.send("<h1>This is Home Page</h1>");
+    res.send("<h1>This is Home Backend Page</h1>");
 })
 
 app.listen(PORT,()=>{
